d76e67f024fc7c36c6acbcffa5f2a329aa5b058f
tduyen.86@gmail.com <tduyen.86@gmail.com>
2021-05-11 13:31:47 +0700

    done

---
+++ src/main/java/com/company/configuration/AppConfig.java
@@ -0,0 +1,21 @@
+package com.company.configuration;
+
+import com.company.security.SecurityWebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityWebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/com/company/security/SecurityAppConfig.java
@@ -0,0 +1,7 @@
+package com.company.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityAppConfig extends AbstractSecurityWebApplicationInitializer {
+
+}
